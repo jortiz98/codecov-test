f4abd1d1e5687c2c7a702c2d0ddbbf52d7696a27
Jacob Ortiz <jortiz@hellosign.com>
2023-02-02 11:43:38 -0500

    add more tests

--- car_test.go
+++ car_test.go
@@ -6,31 +6,31 @@ import (
 	"testing"
 )
 
-func TestCar_GetMaker(t *testing.T) {
-	testcase := []struct {
-		name string
-		car  *foobar.Car
-	}{
-		{name: "BMW", car: &foobar.Car{
-			Miles: 0,
-			Year:  0,
-			Model: "M3",
-			Maker: "BMW",
-		}},
-		{name: "Audi", car: &foobar.Car{
-			Miles: 0,
-			Year:  0,
-			Model: "r8",
-			Maker: "Audi",
-		}},
-		{name: "Tesla", car: &foobar.Car{
-			Miles: 0,
-			Year:  0,
-			Model: "Model Y",
-			Maker: "Tesla",
-		}},
-	}
+var testcase = []struct {
+	name string
+	car  *foobar.Car
+}{
+	{name: "BMW", car: &foobar.Car{
+		Miles: 12951,
+		Year:  1985,
+		Model: "M3",
+		Maker: "BMW",
+	}},
+	{name: "Audi", car: &foobar.Car{
+		Miles: 291000,
+		Year:  2001,
+		Model: "r8",
+		Maker: "Audi",
+	}},
+	{name: "Tesla", car: &foobar.Car{
+		Miles: 6540,
+		Year:  2022,
+		Model: "Model Y",
+		Maker: "Tesla",
+	}},
+}
 
+func TestCar_GetMaker(t *testing.T) {
 	for _, tc := range testcase {
 		t.Run(tc.name, func(t *testing.T) {
 			actual := tc.car.GetMaker()
@@ -42,30 +42,6 @@ func TestCar_GetMaker(t *testing.T) {
 }
 
 func TestCar_GetModel(t *testing.T) {
-	testcase := []struct {
-		name string
-		car  *foobar.Car
-	}{
-		{name: "BMW", car: &foobar.Car{
-			Miles: 0,
-			Year:  0,
-			Model: "M3",
-			Maker: "BMW",
-		}},
-		{name: "Audi", car: &foobar.Car{
-			Miles: 0,
-			Year:  0,
-			Model: "r8",
-			Maker: "Audi",
-		}},
-		{name: "Tesla", car: &foobar.Car{
-			Miles: 0,
-			Year:  0,
-			Model: "Model Y",
-			Maker: "Tesla",
-		}},
-	}
-
 	for _, tc := range testcase {
 		t.Run(tc.name, func(t *testing.T) {
 			actual := tc.car.GetModel()
@@ -74,4 +50,24 @@ func TestCar_GetModel(t *testing.T) {
 		})
 	}
 
+}
+
+func TestCar_GetMiles(t *testing.T) {
+	for _, tc := range testcase {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := tc.car.GetMiles()
+
+			assert.Equal(t, tc.car.Miles, actual)
+		})
+	}
+}
+
+func TestCar_GetYear(t *testing.T) {
+	for _, tc := range testcase {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := tc.car.GetYear()
+
+			assert.Equal(t, tc.car.Year, actual)
+		})
+	}
 }
